fce6857a46e3607faf9e93db626e4922e8391433
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2018-04-18 17:45:25 +0200

    add fastlane

--- Automatic Octo ParakeetUITests/Automatic_Octo_ParakeetUITests.swift
+++ Automatic Octo ParakeetUITests/Automatic_Octo_ParakeetUITests.swift
@@ -18,7 +18,9 @@ class Automatic_Octo_ParakeetUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -33,6 +35,7 @@ class Automatic_Octo_ParakeetUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.tap()
         
+        snapshot("0Launch")
     }
     
 }
