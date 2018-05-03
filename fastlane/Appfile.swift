a5102b34f38f1bb73e84a0c1142659e8d7c57f09
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2018-05-03 16:20:03 +0200

    remove fastlane and update scripts

--- fastlane/Appfile.swift
+++
@@ -1,7 +0,0 @@
-var appIdentifier: String { return "com.jug.summer.camp.Automatic-Octo-Parakeet" } // The bundle identifier of your app
-var appleID: String { return "[[APPLE_ID]]" } // Your Apple email address
-
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
